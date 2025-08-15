df20fc26133abd221ee8e319ec92b2e4523a35cd
MR-Spagetty <54694556+MR-Spagetty@users.noreply.github.com>
2025-08-15 14:01:41 +1200

    added compliance tests for json strings to check handling of escape sequences

---
+++ src/test/java/ecs/engr302/team14/gothim/persistancy/JsonStringComplianceTests.java
@@ -0,0 +1,29 @@
+package ecs.engr302.team14.gothim.persistancy;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class JsonStringComplianceTests {
+    @Test
+    void newLines() {
+        String val = """
+                the quick brown
+                fox jumps
+                over the lazy dogs
+                """;
+        String exp = "the quick brown\\nfox jumps\\nover the lazy dogs";
+        JsonString str = new JsonString(val);
+        assertEquals(val, str.value());
+        assertEquals(exp, val.toString());
+    }
+
+    @Test
+    void otherEscapes() {
+        String val = "\"\t\\\b\f\r";
+        String exp = "\\\"\\t\\\\\\b\\f\\r";
+        JsonString str = new JsonString(val);
+        assertEquals(val, str.value());
+        assertEquals(exp, val.toString());
+    }
+}

5ec342aba91b924b43c60a3203f67a489f507b67
MR-Spagetty <54694556+MR-Spagetty@users.noreply.github.com>
2025-08-15 13:25:13 +1200

    basic JSON object tests

---
+++ src/test/java/ecs/engr302/team14/gothim/persistancy/JsonObjectTests.java
@@ -0,0 +1,49 @@
+package ecs.engr302.team14.gothim.persistancy;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+import java.util.stream.IntStream;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Testing of the implementation JsonObject.
+ *
+ * @author MR-Spagetty
+ */
+public class JsonObjectTests {
+    @Test
+    void testInitialState() {
+        JsonObject obj = new JsonObject();
+        assertEquals(0, obj.size());
+        assertTrue(obj.put("blah", JsonValue.NULL).isEmpty());
+    }
+
+    @Test
+    void testPut() {
+        JsonObject obj = new JsonObject();
+        assertEquals(0, obj.size());
+        obj.put("blah", JsonBool.of(true));
+        assertEquals(1, obj.size());
+        Optional<JsonType> val = obj.put("blah", JsonBool.of(false));
+        assertTrue(val.isPresent());
+        assertEquals(JsonBool.of(true), val.get());
+        Optional<JsonType> ret = obj.put("2", JsonValue.NULL);
+        assertEquals(2, obj.size());
+        assertFalse(ret.isPresent());
+    }
+
+    @Test
+    void testGet() {
+        JsonObject obj = new JsonObject();
+        IntStream.range(-20, 20).mapToDouble(i -> i).forEach(i -> obj.put("" + i, new JsonNum(i)));
+        IntStream.range(-20, 20).mapToDouble(i -> i).forEach(i -> {
+            Optional<JsonType> ret = obj.get("" + i);
+            assertTrue(ret.isPresent());
+            assertEquals(i, ret.get());
+        });
+        assertFalse(obj.get("other").isPresent());
+    }
+}

19f56648a7e4962a41c82ac35e139c039aad12d2
MR-Spagetty <54694556+MR-Spagetty@users.noreply.github.com>
2025-08-26 20:12:15 +1200

    removed unused List import in JsonObject

--- src/main/java/ecs/engr302/team14/gothim/persistancy/JsonObject.java
+++ src/main/java/ecs/engr302/team14/gothim/persistancy/JsonObject.java
@@ -2,7 +2,6 @@ package ecs.engr302.team14.gothim.persistancy;
 
 import java.util.Collection;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;

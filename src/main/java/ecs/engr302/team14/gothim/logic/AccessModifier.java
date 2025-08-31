01c9a9ba46172a88d8b85e85d6e0bbde5092a641
MR-Spagetty <54694556+MR-Spagetty@users.noreply.github.com>
2025-08-31 17:29:36 +1200

    implimented clue

---
+++ src/main/java/ecs/engr302/team14/gothim/logic/AccessModifier.java
@@ -0,0 +1,10 @@
+package ecs.engr302.team14.gothim.logic;
+
+/**
+ * Simple enum for storing the access modifiers for ease of use.
+ *
+ * @author MR-Spagetty
+ */
+public enum AccessModifier {
+    Public, Private, Static;
+}

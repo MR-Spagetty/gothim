5864ab480edf0c229b689d56833d84446f465b37
MR-Spagetty <54694556+MR-Spagetty@users.noreply.github.com>
2025-09-12 12:42:02 +1200

    created clue holder

---
+++ src/main/java/ecs/engr302/team14/gothim/logic/ClueHolder.java
@@ -0,0 +1,105 @@
+package ecs.engr302.team14.gothim.logic;
+
+import ecs.engr302.team14.gothim.persistancy.annotations.DeserializationMethod;
+import ecs.engr302.team14.gothim.persistancy.annotations.SerializedField;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.function.Consumer;
+
+/**
+ * Clue holder class for storing the clues and found clues for the level.
+ *
+ * @author MR-Spagetty
+ */
+public class ClueHolder {
+    @SerializedField
+    private List<Clue> clues = new ArrayList<>();
+    @SerializedField
+    private Set<String> foundClues = new HashSet<>();
+
+    Consumer<String> clueObserver = unused -> {
+    };
+
+    /**
+     * Creates a new holder with the given clues and no clues found already.
+     *
+     * @param clues the clues to store in this holder
+     * @see #ClueHolder(List, List)
+     */
+    public ClueHolder(List<Clue> clues) {
+        this(clues, List.of());
+    }
+
+    /**
+     * Creates a new Clue holder with the given clues and found clues.
+     *
+     * @param clues      the clues to hold
+     * @param foundClues the clue ids that have already been found
+     * @throws IllegalArgumentException if the found clues contains any ids that are
+     *                                  not present in the clues
+     */
+    @DeserializationMethod(serialFieldNames = { "clues", "foundClues" })
+    public ClueHolder(List<Clue> clues, List<String> foundClues) {
+        this.clues.addAll(clues);
+        List<String> invalidFound = foundClues.stream()
+                .filter(id -> clues.parallelStream().anyMatch(c -> !id.equals(c.id()))).toList();
+        if (!invalidFound.isEmpty()) {
+            throw new IllegalArgumentException(
+                    "Some clues stated ot be found do not exist:\n" + invalidFound.toString());
+        }
+        this.foundClues.addAll(foundClues);
+    }
+
+    /**
+     * Marks the specified clue id as found.
+     *
+     * @param id the id of the clue to mark as found
+     * @throws IllegalArgumentException if the clue id does not correspond to a clue
+     */
+    public void findClue(String id) {
+        if (foundClues.contains(id)) {
+            return;
+        }
+        if (clues.parallelStream().noneMatch(c -> c.id().equals(id))) {
+            throw new IllegalArgumentException("Unknown clue: " + id);
+        }
+        foundClues.add(id);
+        clueObserver.accept(id);
+    }
+
+    /**
+     * sets the observer on this holder to the given observer.
+     *
+     * <p>Effectively equivalent to clearing then adding but slightly different.
+     *
+     * @param cons the new observer
+     */
+    public void setObserver(Consumer<String> cons) {
+        this.clueObserver = Objects.requireNonNull(cons);
+    }
+
+    /**
+     * Adds an additional observer to this holder.
+     *
+     * @param cons the new observer to add
+     */
+    public void addObserver(Consumer<String> cons) {
+        Consumer<String> oldCons = this.clueObserver;
+        this.clueObserver = c -> {
+            cons.accept(c);
+            oldCons.accept(c);
+        };
+    }
+
+    /**
+     * Clears all observers from this holder.
+     */
+    public void clearObservers() {
+        this.clueObserver = unused -> {
+        };
+    }
+
+}

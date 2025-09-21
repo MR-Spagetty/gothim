2f0059d0d8bce947bcfecb87bdefbb4408a17c43
MR-Spagetty <54694556+MR-Spagetty@users.noreply.github.com>
2025-09-22 10:47:31 +1200

    Fixed style of Board

--- src/main/java/ecs/engr302/team14/gothim/map/Board.java
+++ src/main/java/ecs/engr302/team14/gothim/map/Board.java
@@ -1,11 +1,5 @@
 package ecs.engr302.team14.gothim.map;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-
 import ecs.engr302.team14.gothim.entities.Entity;
 import ecs.engr302.team14.gothim.entities.NPC;
 import ecs.engr302.team14.gothim.entities.Player;
@@ -13,24 +7,33 @@ import ecs.engr302.team14.gothim.persistancy.annotations.DeserializationMethod;
 import ecs.engr302.team14.gothim.persistancy.annotations.SerializedField;
 import ecs.engr302.team14.gothim.tiles.PrimitiveTile;
 import ecs.engr302.team14.gothim.util.Point;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
+/**
+ * Class for holding the map data for the game.
+ */
 public class Board {
 
     Map<Point, PrimitiveTile> board = new HashMap<>();
-    @SerializedField(deserialParamName="tiles")
+    @SerializedField(deserialParamName = "tiles")
     List<PrimitiveTile> allTiles;
     List<Player> players = new ArrayList<>();
     Map<String, NPC> npcs = new HashMap<>();
-    @SerializedField(deserialParamName="entities")
+    @SerializedField(deserialParamName = "entities")
     List<Entity> allEntities;
     
     /**
-     * 
-     * @param tiles
-     * @param entities
+     * Creates a new board from the given tiles and entities.
+     *
+     * @param tiles the tiles for the map
+     * @param entities the entities within the map
      */
     @DeserializationMethod(serialFieldNames = { "tiles", "entities" })
-    Board(List<PrimitiveTile> tiles , List<Entity> entities){
+    Board(List<PrimitiveTile> tiles, List<Entity> entities) {
         this.allTiles = new ArrayList<>(tiles);
         this.allTiles.stream().forEach(t -> this.board.put(t.pos(), t));
         this.allEntities = new ArrayList<>(entities);
@@ -46,20 +49,33 @@ public class Board {
 
     /*---------------------- Board Stuff -----------------------*/
 
-    public Map<Point, PrimitiveTile> getBoard(){
+    public Map<Point, PrimitiveTile> getBoard() {
         return board;
     }
 
-     /*---------------------- Tile Stuff -----------------------*/
+    /*---------------------- Tile Stuff -----------------------*/
 
-    public PrimitiveTile getTile(Point p){
+    /**
+     * Gets the tile at the given point.
+     *
+     * @param p the point to get the tile at
+     * @return the tile at that point
+     */
+    public PrimitiveTile getTile(Point p) {
         return board.get(p);
     }
 
-    public List<PrimitiveTile> getTiles(Point topleft, Point bottomRight){
+    /**
+     * Gets all tiles within the given rectangle.
+     *
+     * @param topleft the top left Point of the rectangle
+     * @param bottomRight the bottom right Point of the rectangle
+     * @return a list of the tiles within that rectangle
+     */
+    public List<PrimitiveTile> getTiles(Point topleft, Point bottomRight) {
         List<PrimitiveTile> ls = new ArrayList<>();
-        for(double x = topleft.x(); x <= bottomRight.x(); x++){
-            for(double y = topleft.y(); y <= bottomRight.y(); y++){
+        for (double x = topleft.x(); x <= bottomRight.x(); x++) {
+            for (double y = topleft.y(); y <= bottomRight.y(); y++) {
                 //get an option from the board and add to list, otherwise do nothing
                 Optional.ofNullable(board.get(new Point(x, y))).ifPresent(ls::add);
             }
@@ -71,24 +87,42 @@ public class Board {
 
     /*---------------------- Player Stuff -----------------------*/
 
-    public List<Player> getPlayers(){
+    /**
+     * Get all the player entities on the map.
+     *
+     * @return list of the players.
+     */
+    public List<Player> getPlayers() {
         return players;
     }
 
-    public Player getPlayer(int id){
+    /**
+     * Get the player with the given ID.
+     *
+     * @param id the id of the player to get.
+     * @return the specified player
+     */
+    public Player getPlayer(int id) {
         return players.get(id);
     }
 
     /*---------------------- NPC Stuff -----------------------*/
     /**
-     * 
-     * @return
+     * Get all NPCs on the map.
+     *
+     * @return map of NPCs with their names as keys.
      */
-    public Map<String, NPC> getNpcs(){
+    public Map<String, NPC> getNpcs() {
         return npcs;
     }
 
-    public NPC getNPC(String name){
+    /**
+     * Get the NPC with the given name.
+     *
+     * @param name the name of the NPC to get.
+     * @return the specified NPC
+     */
+    public NPC getNPC(String name) {
         return npcs.get(name);
     }
 

cb336922b507ebc29c58604bda0653931a7a27f8
MR-Spagetty <54694556+MR-Spagetty@users.noreply.github.com>
2025-09-22 10:59:10 +1200

    Fixed style of GameState

--- src/main/java/ecs/engr302/team14/gothim/app/GameState.java
+++ src/main/java/ecs/engr302/team14/gothim/app/GameState.java
@@ -1,8 +1,9 @@
 package ecs.engr302.team14.gothim.app;
 
+/**
+ * Enum for keeping track of the current state of the game.
+ */
 public enum GameState {
     Menu,
     Playing;
-
-    private GameState(){}
 }

2b0ddbf65bb879f3626a1c00d7cf5cc27f474cbe
MR-Spagetty <54694556+MR-Spagetty@users.noreply.github.com>
2025-09-22 10:23:02 +1200

    Created Level holder

---
+++ src/main/java/ecs/engr302/team14/gothim/logic/LevelHolder.java
@@ -0,0 +1,21 @@
+package ecs.engr302.team14.gothim.logic;
+
+import ecs.engr302.team14.gothim.map.Board;
+import ecs.engr302.team14.gothim.persistancy.annotations.DeserializationMethod;
+import ecs.engr302.team14.gothim.persistancy.annotations.SerializedField;
+
+/**
+ * Holds all the information for a level.
+ *
+ * @param levelID The ID of the level
+ * @param map The map of the level
+ * @param clues The clues for the level
+ */
+public record LevelHolder(
+        @SerializedField String levelID,
+        @SerializedField Board map,
+        @SerializedField ClueHolder clues) {
+
+    @DeserializationMethod(serialFieldNames = {"levelID", "map", "clues"})
+    public LevelHolder {}
+}

139cfa465ed34de20caf4481663701212ab80575
MR-Spagetty <54694556+MR-Spagetty@users.noreply.github.com>
2025-09-26 18:02:35 +1200

    moved entities to level holder

--- src/main/java/ecs/engr302/team14/gothim/logic/LevelHolder.java
+++ src/main/java/ecs/engr302/team14/gothim/logic/LevelHolder.java
@@ -1,9 +1,17 @@
 package ecs.engr302.team14.gothim.logic;
 
+import ecs.engr302.team14.gothim.entities.Entity;
+import ecs.engr302.team14.gothim.entities.Player;
 import ecs.engr302.team14.gothim.map.Board;
 import ecs.engr302.team14.gothim.map.MapBuilder;
 import ecs.engr302.team14.gothim.persistancy.annotations.DeserializationMethod;
 import ecs.engr302.team14.gothim.persistancy.annotations.SerializedField;
+import ecs.engr302.team14.gothim.util.Point;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Holds all the information for a level.
@@ -12,14 +20,66 @@ import ecs.engr302.team14.gothim.persistancy.annotations.SerializedField;
  * @param map The map of the level
  * @param clues The clues for the level
  */
-public record LevelHolder(
-        @SerializedField String levelID,
+public record LevelHolder(@SerializedField String levelID,
         @SerializedField(deserialParamName = "map") MapBuilder mapBuilder,
         Board map,
-        @SerializedField ClueHolder clues) {
+        @SerializedField ClueHolder clues,
+        @SerializedField ArrayList<Entity> entities,
+        @SerializedField ArrayList<Player> players,
+        @SerializedField Point spawnPoint) {
 
-    @DeserializationMethod(serialFieldNames = {"levelID", "map", "clues"})
-    public LevelHolder(String levelID, MapBuilder mapBuilder, ClueHolder clues) {
-        this(levelID, mapBuilder, mapBuilder.build(), clues);
+    /**
+     * Creates a new LevelHolder. DO NOT USE DIRECTLY,
+     * instead please use {@link #LevelHolder(String, MapBuilder, ClueHolder, List, List, Point)}.
+     *
+     * @param levelID the ID of the level
+     * @param mapBuilder the map builder used to create the map
+     * @param map the map of the level
+     * @param clues the clues for the level
+     * @param entities the entities within the level
+     * @param players the players within the level
+     * @param spawnPoint the spawn point for the level
+     */
+    public LevelHolder {
+        Objects.requireNonNull(levelID);
+        Objects.requireNonNull(mapBuilder);
+        Objects.requireNonNull(map);
+        Objects.requireNonNull(clues);
+        Objects.requireNonNull(entities);
+        Objects.requireNonNull(players);
+        Objects.requireNonNull(spawnPoint);
+        Stream.concat(entities.stream(), players.stream())
+                .parallel()
+                .forEach(e -> map.getTile(e.getPosition()).setOcupant(e));
+        String mismatchedEntities = Stream.concat(entities.stream(), players.stream())
+                .parallel()
+                .filter(e -> map.getTile(e.getPosition()).getOcupant().orElse(null) == e)
+                .map(Object::toString)
+                .collect(Collectors.joining("\n"));
+        if (!mismatchedEntities.isBlank()) {
+            throw new IllegalStateException(
+                "The following entities were not placed on the map correctly:\n"
+                + mismatchedEntities
+                );
+        }
+    }
+
+    /**
+     * Creates a new LevelHolder, constructing the map using hte provided builder.
+     *
+     * @param levelID the ID of the level
+     * @param mapBuilder the map builder used to create the map
+     * @param clues the clues for the level
+     * @param entities the entities within the level
+     * @param players the players within the level
+     * @param spawnPoint the spawn point for the level
+     */
+    @DeserializationMethod(serialFieldNames = {
+        "levelID", "map", "clues", "entities", "players", "spawnPoint"
+    })
+    public LevelHolder(String levelID, MapBuilder mapBuilder, ClueHolder clues,
+            List<Entity> entities, List<Player> players, Point spawnPoint) {
+        this(levelID, mapBuilder, mapBuilder.build(), clues, new ArrayList<>(entities),
+                new ArrayList<>(players), spawnPoint);
     }
 }
--- src/main/java/ecs/engr302/team14/gothim/map/Board.java
+++ src/main/java/ecs/engr302/team14/gothim/map/Board.java
@@ -1,16 +1,12 @@
 package ecs.engr302.team14.gothim.map;
 
-import ecs.engr302.team14.gothim.entities.Entity;
-import ecs.engr302.team14.gothim.entities.NPC;
-import ecs.engr302.team14.gothim.entities.Player;
-import ecs.engr302.team14.gothim.persistancy.annotations.DeserializationMethod;
-import ecs.engr302.team14.gothim.persistancy.annotations.SerializedField;
 import ecs.engr302.team14.gothim.tiles.PrimitiveTile;
 import ecs.engr302.team14.gothim.util.Point;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 
 /**
@@ -18,45 +14,11 @@ import java.util.Optional;
  */
 public class Board {
 
-    Map<Point, PrimitiveTile> board = new HashMap<>();
-    @SerializedField(deserialParamName = "tiles")
-    List<PrimitiveTile> allTiles;
-    List<Player> players = new ArrayList<>();
-    Map<String, NPC> npcs = new HashMap<>();
-    @SerializedField(deserialParamName = "entities")
-    List<Entity> allEntities;
-    
-    /**
-     * Creates a new board from the given tiles and entities.
-     *
-     * @param tiles the tiles for the map
-     * @param entities the entities within the map
-     */
-    @DeserializationMethod(serialFieldNames = { "tiles", "entities" })
-    Board(List<PrimitiveTile> tiles, List<Entity> entities) {
-        this.allTiles = new ArrayList<>(tiles);
-        this.allTiles.stream().forEach(t -> {
-            this.board.put(t.pos(), t);
-            t.linkMap(this);
-        });
-        this.allEntities = new ArrayList<>(entities);
-        this.allEntities.forEach(e -> {
-            switch (e) {
-                case NPC npc -> this.npcs.put(npc.getName(), npc);
-                case Player p -> this.players.add(p);
-                default -> {
-                }
-            }
-        });
+    Map<Point, PrimitiveTile> board;
+
+    Board(Map<Point, PrimitiveTile> board) {
+        this.board = new HashMap<>(Objects.requireNonNull(board));
     }
-
-    /*---------------------- Board Stuff -----------------------*/
-
-    public Map<Point, PrimitiveTile> getBoard() {
-        return board;
-    }
-
-    /*---------------------- Tile Stuff -----------------------*/
 
     /**
      * Gets the tile at the given point.
@@ -85,49 +47,4 @@ public class Board {
         }
         return ls;
     }
-
-
-
-    /*---------------------- Player Stuff -----------------------*/
-
-    /**
-     * Get all the player entities on the map.
-     *
-     * @return list of the players.
-     */
-    public List<Player> getPlayers() {
-        return players;
-    }
-
-    /**
-     * Get the player with the given ID.
-     *
-     * @param id the id of the player to get.
-     * @return the specified player
-     */
-    public Player getPlayer(int id) {
-        return players.get(id);
-    }
-
-    /*---------------------- NPC Stuff -----------------------*/
-    /**
-     * Get all NPCs on the map.
-     *
-     * @return map of NPCs with their names as keys.
-     */
-    public Map<String, NPC> getNpcs() {
-        return npcs;
-    }
-
-    /**
-     * Get the NPC with the given name.
-     *
-     * @param name the name of the NPC to get.
-     * @return the specified NPC
-     */
-    public NPC getNPC(String name) {
-        return npcs.get(name);
-    }
-
-
 }

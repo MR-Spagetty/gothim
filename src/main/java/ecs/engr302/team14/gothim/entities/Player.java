239618183b7832a8e38a34ad9c17f0fa10a7d782
MR-Spagetty <54694556+MR-Spagetty@users.noreply.github.com>
2025-09-29 21:04:54 +1300

    Created disguise item and made player part of a family

--- src/main/java/ecs/engr302/team14/gothim/entities/Player.java
+++ src/main/java/ecs/engr302/team14/gothim/entities/Player.java
@@ -1,5 +1,7 @@
 package ecs.engr302.team14.gothim.entities;
 
+import ecs.engr302.team14.gothim.logic.Family;
+import ecs.engr302.team14.gothim.persistancy.annotations.SerializedField;
 import ecs.engr302.team14.gothim.util.Point;
 import java.awt.Color;
 import java.awt.Graphics;
@@ -11,48 +13,14 @@ import java.util.Set;
  * Basic player class.
  */
 public class Player extends Entity {
-    private int accessLevel = 0; // default to "public" (change to an ENUM)
-    private Area currentArea;
-    private int speed = 4;
-    private int size = 32; // for drawing
     private BufferedImage sprite;
 
-    public Player(String name, Point position) {
+    @SerializedField
+    private final Family family;
+
+    public Player(String name, Point position, Family fam) {
         super(name, position);
-    }
-
-    /**
-     * moves this palyer to a new area.
-     *
-     * @param newArea the new area
-     */
-    public void moveTo(Area newArea) {
-        if (this.canEnter(newArea)) {
-            this.currentArea = newArea;
-            System.out.println(name + " moved to " + newArea.getName());
-        } else {
-            System.out.println("Access denied! Need higher clearance");
-        }
-    }
-
-    protected boolean canEnter(Area area) {
-        return this.accessLevel >= area.getRequiredAccess();
-    }
-
-    public Area getCurrentArea() {
-        return currentArea;
-    }
-
-    public void setCurrentArea(Area area) {
-        this.currentArea = area;
-    }
-
-    public int getAccessLevel() {
-        return this.accessLevel;
-    }
-
-    public void setAccessLevel(int level) {
-        this.accessLevel = level;
+        this.family = fam;
     }
 
     /**
@@ -67,24 +35,17 @@ public class Player extends Entity {
         double newY = position.y();
 
         if (pressedKeys.contains(KeyEvent.VK_W)) {
-            newY -= speed;
+            newY--;
         }
         if (pressedKeys.contains(KeyEvent.VK_S)) {
-            newY += speed;
+            newY++;
         }
         if (pressedKeys.contains(KeyEvent.VK_A)) {
-            newX -= speed;
+            newX--;
         }
         if (pressedKeys.contains(KeyEvent.VK_D)) {
-            newX += speed;
+            newX++;
         }
-
-        // Clamp inside bounds
-        newX = Math.max(0, Math.min(newX, screenWidth - size));
-        newY = Math.max(0, Math.min(newY, screenHeight - size));
-
-        // Create a new point instance with updated coords (not sure if this is the best
-        // approach)
         position = new Point(newX, newY);
     }
 
@@ -99,7 +60,7 @@ public class Player extends Entity {
         } else {
             // fallback: draw a placeholder rectangle
             g.setColor(Color.WHITE);
-            g.fillRect((int) position.x(), (int) position.y(), size, size);
+            g.fillRect((int) position.x(), (int) position.y(), 40, 40);
         }
     }
 }

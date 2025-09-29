7f9be45ea8fb3287a7b09b8949c221c3ebac5b65
MR-Spagetty <54694556+MR-Spagetty@users.noreply.github.com>
2025-09-29 20:14:01 +1300

    Fixed style

--- src/main/java/ecs/engr302/team14/gothim/entities/InteractableEntity.java
+++ src/main/java/ecs/engr302/team14/gothim/entities/InteractableEntity.java
@@ -2,9 +2,26 @@ package ecs.engr302.team14.gothim.entities;
 
 import ecs.engr302.team14.gothim.util.Point;
 
+/**
+ * Abstract class that covers all intractable entities.
+ */
 public abstract class InteractableEntity extends Entity {
 
     public InteractableEntity(String name, Point position) {
         super(name, position);
     }
+
+    /**
+     * Checks if the player is within 1 tile of this NPC.
+     *
+     * @param player the player to check against
+     * @return if the player is within interaction range
+     */
+    public boolean isNear(Player player) {
+        int dx = (int) (position.x() - player.getPosition().x());
+        int dy = (int) (position.y() - player.getPosition().y());
+        return Math.abs(dx) <= 1 && Math.abs(dy) <= 1;
+    }
+
+    public abstract void interact(Player p);
 }
--- src/main/java/ecs/engr302/team14/gothim/entities/NPC.java
+++ src/main/java/ecs/engr302/team14/gothim/entities/NPC.java
@@ -1,41 +1,34 @@
 package ecs.engr302.team14.gothim.entities;
+
 import ecs.engr302.team14.gothim.util.Point;
-
-import javax.imageio.ImageIO;
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
 
-public class NPC extends InteractableEntity{
+/**
+ * Basic NPC class.
+ */
+public class NPC extends InteractableEntity {
 
     private String dialogue;
     private BufferedImage sprite;
     private int size = 40;
 
-    public NPC(String name, String dialogue, Point position){
+    public NPC(String name, String dialogue, Point position) {
         super(name, position);
         this.dialogue = dialogue;
-
-//        try {
-//            sprite = ImageIO.read(getClass().getResource("/assets/npc.png"));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
     }
 
     //Complete interaction
-    public void interact() {
-        System.out.println(name+": " + dialogue);
+    public void interact(Player p) {
+        System.out.println(name + ": " + dialogue);
     }
 
-    public boolean isNear(Player player) {
-        int dx = (int) (position.x() - player.getPosition().x());
-        int dy = (int) (position.y() - player.getPosition().y());
-        int distanceSquared = dx * dx + dy * dy;
-        int interactionRange = 40; // pixels
-        return distanceSquared < interactionRange * interactionRange;
-    }
-
+    /**
+     * Renders the NPC on the given graphics.
+     *
+     * @param g the graphics to render the NPC on
+     */
     public void render(Graphics g) {
         if (sprite != null) {
             g.drawImage(sprite, (int) position.x(), (int) position.y(), 50, 50, null);

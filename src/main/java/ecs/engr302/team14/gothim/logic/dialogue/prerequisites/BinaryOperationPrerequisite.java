8e36ace7223cf9028f4d902d25f7d081b40c9e8b
MR-Spagetty <54694556+MR-Spagetty@users.noreply.github.com>
2025-09-29 18:57:23 +1300

    fixed style of BinaryOperationPrerequisite.java

--- src/main/java/ecs/engr302/team14/gothim/logic/dialogue/prerequisites/BinaryOperationPrerequisite.java
+++ src/main/java/ecs/engr302/team14/gothim/logic/dialogue/prerequisites/BinaryOperationPrerequisite.java
@@ -2,7 +2,13 @@ package ecs.engr302.team14.gothim.logic.dialogue.prerequisites;
 
 import ecs.engr302.team14.gothim.persistancy.annotations.SerializedField;
 
-public abstract class BinaryOperationPrerequisite implements DialoguePrerequisite{
+/**
+ * Abstract class to minimise repeated code for binary operation  dialogue prerequisites
+ * such as "Or" and "And".
+ *
+ * @author MR-Spagetty
+ */
+public abstract class BinaryOperationPrerequisite implements DialoguePrerequisite {
     @SerializedField(deserialParamName = "a")
     final DialoguePrerequisite opA;
     @SerializedField(deserialParamName = "b")

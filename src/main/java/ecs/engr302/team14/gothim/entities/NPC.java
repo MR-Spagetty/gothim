c509f795d0b103fbfee3d9d0ffe0ce696d76667c
MR-Spagetty <54694556+MR-Spagetty@users.noreply.github.com>
2025-10-09 13:26:33 +1300

    removed other rendering stuff form NPC class

--- src/main/java/ecs/engr302/team14/gothim/entities/NPC.java
+++ src/main/java/ecs/engr302/team14/gothim/entities/NPC.java
@@ -6,9 +6,6 @@ import ecs.engr302.team14.gothim.persistancy.annotations.DeserializationMethod;
 import ecs.engr302.team14.gothim.persistancy.annotations.SerializationExtends;
 import ecs.engr302.team14.gothim.persistancy.annotations.SerializedField;
 import ecs.engr302.team14.gothim.util.Point;
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.image.BufferedImage;
 
 /**
  * Basic NPC class.
@@ -18,8 +15,6 @@ public class NPC extends InteractableEntity {
 
     @SerializedField
     private Dialogue dialogue;
-    private BufferedImage sprite;
-    private int size = 40;
 
     @DeserializationMethod(serialFieldNames = { "name", "dialogue", "pos" })
     public NPC(String name, Dialogue dialogue, Point position) {
@@ -30,20 +25,5 @@ public class NPC extends InteractableEntity {
     //Complete interaction
     public void interact(Player p) {
         Main.dialogue(this.name, this.dialogue);
-    }
-
-    /**
-     * Renders the NPC on the given graphics.
-     *
-     * @param g the graphics to render the NPC on
-     */
-    public void render(Graphics g) {
-        if (sprite != null) {
-            g.drawImage(sprite, (int) position.x(), (int) position.y(), 50, 50, null);
-        } else {
-            // fallback: draw a placeholder rectangle
-            g.setColor(Color.WHITE);
-            g.fillRect((int) position.x(), (int) position.y(), size, size);
-        }
     }
 }
